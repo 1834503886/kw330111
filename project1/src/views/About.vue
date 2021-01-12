5ef353303f185f3b3f585d47b01d93fc27ea506b
83590174@qq.com <Gu83590174>
2021-01-12 09:12:59 +0800

    第一次提交

---
+++ project1/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   <h2>关于我们的页面</h2>
+   <h3>{{num}}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        num: 100
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
